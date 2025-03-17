589a64f9c7699b3e8805461e38eca5f33b0d3828
1mpulseee <83034758+1mpulseee@users.noreply.github.com>
2025-03-17 09:53:38 +0300

    refactoring and additional functions

--- Assets/Scripts/MessageSettings.cs
+++ Assets/Scripts/MessageSettings.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewMessageSettings", menuName = "Message Settings", order = 51)]
 public class MessageSettings : ScriptableObject
 {
-    [SerializeField] private float betweenHalf = 0.05f;
-    [SerializeField] private float betweenChar = 0.03f;
+    [SerializeField] private float delayBetweenHalves = 0.05f;
+    [SerializeField] private float delayBetweenCharacters = 0.03f;
     [SerializeField] private float smoothTime = 0.1f;
 
     [Space(25)]
     [SerializeField] private bool isPaused = false;
     [SerializeField] private bool autoSkip = false;
-    [SerializeField] private float skipDelay = 0.3f;
+    [SerializeField] private float skipDelay = 0.5f;
 
     [Space(25)]
     [SerializeField] private string[] messages;
 
-    public float BetweenHalf => betweenHalf;
-    public float BetweenChar => betweenChar;
+    public float DelayBetweenHalves => delayBetweenHalves;
+    public float DelayBetweenCharacters => delayBetweenCharacters;
     public float SmoothTime => smoothTime;
     public bool IsPaused => isPaused;
     public bool AutoSkip => autoSkip;
